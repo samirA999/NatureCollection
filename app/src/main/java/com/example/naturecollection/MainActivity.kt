e07b13351d1b65fc9c2e31dc8e1a2c024dd4098b
mama <oussa41@live.fr>
2022-08-03 17:51:43 +0200

    ajout des photos

--- app/src/main/java/com/example/naturecollection/MainActivity.kt
+++ app/src/main/java/com/example/naturecollection/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.naturecollection
 import android.os.Bundle
 
 import androidx.appcompat.app.AppCompatActivity
+import com.example.naturecollection.fragments.HomeFragment
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,5 +21,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun setContenView(activityMain: Int) {
 
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container,HomeFragment())
+        transaction.addToBackStack(null)
+
     }
 }
+
