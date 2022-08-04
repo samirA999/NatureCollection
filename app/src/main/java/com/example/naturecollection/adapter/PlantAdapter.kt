a6624cc083c0194ea5acd28af409dd3b696b4cc0
mama <oussa41@live.fr>
2022-08-04 13:20:22 +0200

    viewholder

--- app/src/main/java/com/example/naturecollection/adapter/PlantAdapter.kt
+++ app/src/main/java/com/example/naturecollection/adapter/PlantAdapter.kt
@@ -1,4 +1,14 @@
 package com.example.naturecollection.adapter
 
+import android.view.View
+
 class PlantAdapter {
+
+
+    //boite pour ranger tout les composants à controler
+
+    class ViewHolder(view: View){
+
+
+    }
 }
