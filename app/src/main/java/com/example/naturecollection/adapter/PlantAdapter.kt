7fd9cf755ef6e7588faaa2db0a26982c9a818a99
mama <oussa41@live.fr>
2022-08-04 16:28:52 +0200

    AdaPt

--- app/src/main/java/com/example/naturecollection/adapter/PlantAdapter.kt
+++ app/src/main/java/com/example/naturecollection/adapter/PlantAdapter.kt
@@ -1,14 +1,37 @@
 package com.example.naturecollection.adapter
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.naturecollection.R
 
-class PlantAdapter {
+class PlantAdapter : RecyclerView.Adapter<PlantAdapter.ViewHolder>() {
 
 
     //boite pour ranger tout les composants à controler
 
-    class ViewHolder(view: View){
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+        //image de la plante ?
+
+        val plantImage = view.findViewById<ImageView>(R.id.image_item)
 
 
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_horizontal_plant)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
     }
 }
