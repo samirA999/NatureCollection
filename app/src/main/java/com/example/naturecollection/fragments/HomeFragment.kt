45c925672c6932559b1976421be285d3905dd9eb
mama <oussa41@live.fr>
2022-08-04 17:20:33 +0200

    homeFra

--- app/src/main/java/com/example/naturecollection/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/naturecollection/fragments/HomeFragment.kt
@@ -5,12 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.naturecollection.R
 
 class HomeFragment : Fragment() {
 
-    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-        return inflater?.inflate(R.layout.fragment_home, container, false)
+    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+
+
+        val view = inflater?.inflate(R.layout.fragment_home, container, false)
+
+        // recuperer le recyclerview
+
+        val horizontalRecyclerView = view.findViewById<RecyclerView>(R.id.horizontal_recycler_view)
+        return view
     }
 }
