d34af81f5608dacd5af41a6fc48e68cc56a0dcf2
mama <oussa41@live.fr>
2022-08-09 12:12:24 +0200

    aj

--- app/src/main/java/com/example/naturecollection/PlantModel.kt
+++ app/src/main/java/com/example/naturecollection/PlantModel.kt
@@ -1,10 +1,13 @@
 package com.example.naturecollection
 
 class PlantModel (
+
     val id:String = "plant0",
     val name: String = "Tulipe",
     val description: String = "petite description",
     val imageUrl: String = "http://graven.yt/plante.jpg",
+    val grow:String ="Faible",
+    val water:String ="Moyenne",
     var liked: Boolean = false
 
 
--- app/src/main/java/com/example/naturecollection/PlantPopup.kt
+++ app/src/main/java/com/example/naturecollection/PlantPopup.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.Window
 import android.widget.ImageView
+import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.example.naturecollection.adapter.PlantAdapter
 
@@ -25,5 +26,18 @@ class PlantPopup (
      // actualiser l'image de la plante
         val plantImage = findViewById<ImageView>(R.id.image_item)
         Glide.with(adapter.context).load(Uri.parse(currentPlant.imageUrl)).into(plantImage)
+
+        //actualiser le nom de la plante
+        findViewById<TextView>(R.id.popup_plant_name).text = currentPlant.name
+
+        //actualiser ala description de la plante
+        findViewById<TextView>(R.id.popup_plant_description_subtitle).text = currentPlant.description
+
+        //actualiser la croissance de la plante
+        findViewById<TextView>(R.id.popup_plant_grow_subtitle).text = currentPlant.grow
+
+
+       //actualiser la croissance deau
+        findViewById<TextView>(R.id.popup_plant_water_subtitle).text = currentPlant.water
     }
 }
