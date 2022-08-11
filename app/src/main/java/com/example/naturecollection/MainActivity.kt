fc55929dda4db95b4c9ebebc425884a185c980bf
mama <oussa41@live.fr>
2022-08-11 13:02:41 +0200

    charger image a partir de mon Télépho

--- app/src/main/java/com/example/naturecollection/MainActivity.kt
+++ app/src/main/java/com/example/naturecollection/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity() {
 
             //injecter le fragment dans notre boite ( fragment_container)
             val transaction = supportFragmentManager.beginTransaction()
-            transaction.replace(R.id.fragment_container, AddPlantFragment(this))
+            transaction.replace(R.id.fragment_container, HomeFragment(this))
             transaction.addToBackStack(null)
             transaction.commit()
 
--- app/src/main/java/com/example/naturecollection/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/naturecollection/fragments/HomeFragment.kt
@@ -26,7 +26,7 @@ class HomeFragment (
         // recuperer le recyclerview
 
         val horizontalRecyclerView = view.findViewById<RecyclerView>(R.id.horizontal_recycler_view)
-        horizontalRecyclerView.adapter = PlantAdapter(context, plantList, R.layout.item_horizontal_plant)
+        horizontalRecyclerView.adapter = PlantAdapter(context, plantList.filter { !it.liked }, R.layout.item_horizontal_plant)
 
 
 
